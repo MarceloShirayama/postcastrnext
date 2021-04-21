c61d0079dd79f62cd4512448be15a82c1b6d8bbf
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-04-21 11:51:48 -0300

    ✨ add examples of consumming APIs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,71 @@
+/*
+3 Formas de consumir uma API no next
+(1 forma funciona em qualquer projeto react as outras duas só funionam no next)
+
+1- Forma SPA - Single Page Application
+2- Forma SSR - Server Side Render
+3- Forma SSG - Static Site Generation (só funciona em produção)
+*/
+
+// 1- Forma SPA - Single Page Application
+/*
+import { useEffect } from "react"
+
 export default function Home() {
+  useEffect(() => {
+    fetch('http://localhost:3333/episodes')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [])
+
   return (
     <h1>Index</h1>
   )
+}
+*/
+
+// 2- Forma SSR - Server Side Render
+/*
+export default function Home(props) {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  )
+}
+
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    }
+  }
+}
+*/
+
+// 3- Forma SSG - Static Site Generator (só funciona em produção)
+
+export default function Home(props) {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, // a cada 8 horas
+  }
 }
